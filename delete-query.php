17b5949397f4f2300d5088949377b2b86c7df29a
AbishekPerera <abhishekperera77@gmail.com>
2022-12-14 01:24:32 +0530

    Delete function

--- delete-query.php
+++ delete-query.php
@@ -1,31 +1,37 @@
+<?php session_start(); ?>
+
 <?php
 require_once('inc/connection.php');
 ?>
 
 <?php
-$query = "DELETE FROM user WHERE id = 5 LIMIT 1";
-$result_set = mysqli_query($connection, $query);
-
-if ($result_set) {
-    echo mysqli_affected_rows($connection);
-} else {
-    echo "fail";
+if (!isset($_SESSION['user_id'])) {
+    header('Location: index.php');
 }
 
+if (isset($_GET['user_id'])) {
+
+    $user_id = mysqli_real_escape_string($connection, $_GET['user_id']);
+
+    if ( $user_id == $_SESSION['user_id'] ) {
+        // should not delete current user
+        header('Location: users.php?err=cannot_delete_current_user');
+    } else {
+        // deleting the user
+        $query = "UPDATE user SET is_delete = 1 WHERE id = {$user_id} LIMIT 1";
+
+        $result = mysqli_query($connection, $query);
+
+        if ($result) {
+            // user deleted
+            header('Location: users.php?msg=user_deleted');
+        } else {
+            header('Location: users.php?err=delete_failed');
+        }
+    }
+    
+} else {
+    header('Location: users.php');
+}
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    <h1>page</h1>
-</body>
-</html>
-
-<?php mysqli_close($connection);?>
